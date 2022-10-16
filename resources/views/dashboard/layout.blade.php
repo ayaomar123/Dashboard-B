d50d1d19302d22158119faacd0ca4d8897851bc9
aya_al_rahman <ayakomar@gmail.com>
2022-10-16 13:11:49 +0300

    auth

---
+++ resources/views/dashboard/layout.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+@include('dashboard.styles')
+
+<body class="bg-theme bg-theme1">
+
+<!-- Start wrapper-->
+<div id="wrapper">
+
+    @include('dashboard.aside')
+
+    @include('dashboard.header')
+
+    <div class="clearfix"></div>
+
+    <div class="content-wrapper">
+        <div class="container-fluid">
+
+            <!--Start Dashboard Content-->
+
+            @yield('content')
+
+            <!--End Dashboard Content-->
+
+            <!--start overlay-->
+            <div class="overlay toggle-menu"></div>
+            <!--end overlay-->
+
+        </div>
+        <!-- End container-fluid-->
+
+    </div><!--End content-wrapper-->
+    <!--Start Back To Top Button-->
+    <a href="javaScript:void();" class="back-to-top"><i class="fa fa-angle-double-up"></i> </a>
+    <!--End Back To Top Button-->
+
+    @include('dashboard.footer')
+    @include('dashboard.colors')
+
+</div><!--End wrapper-->
+
+<!-- Bootstrap core JavaScript-->
+@include('dashboard.scripts')
+
+
+</body>
+</html>
